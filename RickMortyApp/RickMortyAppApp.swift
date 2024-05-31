a834a41bf8b000d25ad0814d8ab56ec57ff7c8e2
BakarKharabadze <kharabadzebaqar@gmail.com>
2024-05-31 11:53:52 +0400

    Initial Commit

---
+++ RickMortyApp/RickMortyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickMortyAppApp.swift
+//  RickMortyApp
+//
+//  Created by Bakar Kharabadze on 5/31/24.
+//
+
+import SwiftUI
+
+@main
+struct RickMortyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
